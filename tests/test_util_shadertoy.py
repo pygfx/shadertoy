fc8c43f4ed431a0d27c3d6cfa24d161b314cd8c5
Jan <jakel101@hhu.de>
2024-02-11 22:05:50 +0100

    Add test for mainImage variants

--- tests/test_util_shadertoy.py
+++ tests/test_util_shadertoy.py
@@ -54,6 +54,53 @@ def test_shadertoy_glsl():
 
     shader._draw_frame()
 
+def test_shadertoy_glsl2():
+    # Import here, because it imports the wgpu.gui.auto
+    from wgpu_shadertoy import Shadertoy
+
+    shader_code = """
+        void mainImage(out vec4 fragColor, vec2 frag_coord) {
+            vec2 uv = frag_coord / i_resolution.xy;
+
+            if ( length(frag_coord - i_mouse.xy) < 20.0 ) {
+                fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+            }else{
+                fragColor = vec4( 0.5 + 0.5 * sin(i_time * vec3(uv, 1.0) ), 1.0);
+            }
+
+        }
+    """
+
+    shader = Shadertoy(shader_code, resolution=(800, 450))
+    assert shader.resolution == (800, 450)
+    assert shader.shader_code == shader_code
+    assert shader.shader_type == "glsl"
+
+    shader._draw_frame()
+
+def test_shadertoy_glsl3():
+    # Import here, because it imports the wgpu.gui.auto
+    from wgpu_shadertoy import Shadertoy
+
+    shader_code = """
+        void    mainImage(out vec4 fragColor, vec2 frag_coord) {
+            vec2 uv = frag_coord / i_resolution.xy;
+
+            if ( length(frag_coord - i_mouse.xy) < 20.0 ) {
+                fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+            }else{
+                fragColor = vec4( 0.5 + 0.5 * sin(i_time * vec3(uv, 1.0) ), 1.0);
+            }
+
+        }
+    """
+
+    shader = Shadertoy(shader_code, resolution=(800, 450))
+    assert shader.resolution == (800, 450)
+    assert shader.shader_code == shader_code
+    assert shader.shader_type == "glsl"
+
+    shader._draw_frame()
 
 def test_shadertoy_offscreen():
     # Import here, because it imports the wgpu.gui.auto
