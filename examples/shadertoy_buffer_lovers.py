2c360680143756cca8ee4b12987f26541ec19129
Jan <jakel101@hhu.de>
2024-11-21 01:05:44 +0100

    fix example

--- examples/shadertoy_buffer_lovers.py
+++ examples/shadertoy_buffer_lovers.py
@@ -4,11 +4,10 @@ from wgpu_shadertoy import Shadertoy
 
 # shadertoy source: https://www.shadertoy.com/view/ssjyWc by FabriceNeyret2 (CC-BY-NC-SA-3.0?)
 
-# current "bug": the string kinda floats off to the upper right corner, without any inputs... ? Likely to be some issue with the implementation of buffers.
 shader_id = "ssjyWc"
 json_path = Path(Path(__file__).parent, f"shader_{shader_id}.json")
 
-shader = Shadertoy.from_json(json_path, resolution=(1024, 512))
+shader = Shadertoy.from_json(json_path, resolution=(800, 450))
 
 if __name__ == "__main__":
     shader.show()
