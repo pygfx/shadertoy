6ed1afa1efa204f3d5783420c0ad218c89c1aba9
Jan <jakel101@hhu.de>
2025-04-03 23:46:38 +0200

    change buffers examples

--- examples/shadertoy_buffer_lovers.py
+++
@@ -1,9 +0,0 @@
-from wgpu_shadertoy import Shadertoy
-
-# shadertoy source: https://www.shadertoy.com/view/ssjyWc by FabriceNeyret2 (CC-BY-NC-SA-3.0?)
-
-# TODO: replace with json so it can run without a API key?
-shader = Shadertoy.from_id("https://www.shadertoy.com/view/ssjyWc", resolution=(800, 450))
-
-if __name__ == "__main__":
-    shader.show()
