049f750fcbb5cf6d870d325eee0cab4ddbca324d
Jan <jakel101@hhu.de>
2025-05-05 14:29:00 -0400

    basic type inference

--- wgpu_shadertoy/imgui.py
+++ wgpu_shadertoy/imgui.py
@@ -1,4 +1,5 @@
 import re
+# from .shadertoy import UniformArray #TODO: likely restructure into a different file for import reasons...
 
 # could imgui become just another RenderPass after Image? I got to understand backend vs renderer first.
 
@@ -18,12 +19,24 @@ def parse_constants(code, common_code):
     define_pattern = re.compile(r"#define\s+(\w+)\s+(.+)")
 
     constants = []
-    for line in code.splitlines():
+    for li, line in enumerate(code.splitlines()):
         match = define_pattern.match(line.rstrip())
         if match:
-            constants.append(match.groups())
             name, value = match.groups()
-            print(f"Found constant: {name} with value: {value}")
+            if "." in value: #value.isdecimal?
+                # TODO: wgsl needs to be more specific (f32 for example?) - but there is no preprocessor anyways...
+                dtype = "float"
+                value = float(value)
+            elif value.isdecimal: # value.isalum?
+                dtype = "int"
+                value = int(value)
+            else:
+                # TODO complexer types?
+                print(f"can't parse type for constant {name} with value {value}, skipping")
+                continue
+            # todo: remove lines here?
+            constants.append((li, name, value, dtype)) # what about line to remove?
+            print(f"In line {li} found constant: {name} with value: {value} of dtype {dtype}")
 
     return constants
 
@@ -33,6 +46,16 @@ def make_uniform(constants):
     # return a UniformArray object (cycling import?)
     # (does this need to be a class to update the values?)
     pass
+
+def make_import(uniform) -> str:
+    # codegen the import block for this uniform (including binding? - which number?)
+    # could be part of the UniformArray class maybe?
+    # to be pasted near the top of the fragment shader code.
+    # alternatively: insert these in the ShadertoyInputs uniform?
+    # better yet: use push constants
+
+    pass
+
 
 # imgui stuff
 def update_gui():
