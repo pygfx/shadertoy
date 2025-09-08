99ebc8fbdc64505a3901485f1e7ed03dfaa839d6
Jan <jakel101@hhu.de>
2025-09-08 21:31:52 +0200

    update ruff rules

--- wgpu_shadertoy/__init__.py
+++ wgpu_shadertoy/__init__.py
@@ -1,5 +1,4 @@
+from ._version import __version__, version_info
 from .inputs import ShadertoyChannel, ShadertoyChannelBuffer, ShadertoyChannelTexture
 from .passes import BufferRenderPass, ImageRenderPass
 from .shadertoy import Shadertoy
-
-from ._version import __version__, version_info
