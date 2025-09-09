e792fb8d3bfbe03c333ef24c2a68dbfc67fe50f9
Jan <jakel101@hhu.de>
2025-09-09 18:33:09 +0200

    only ignore reformatting

--- wgpu_shadertoy/__init__.py
+++ wgpu_shadertoy/__init__.py
@@ -1,3 +1,5 @@
+# ruff: noqa: F401
+
 from ._version import __version__, version_info
 from .inputs import ShadertoyChannel, ShadertoyChannelBuffer, ShadertoyChannelTexture
 from .passes import BufferRenderPass, ImageRenderPass
